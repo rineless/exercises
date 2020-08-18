d7d2de07106db87abb7bdfaf9aabbc1e0312c644
rine <krineless@gmail.com>
2020-08-19 01:42:20 +0200

    Fix #5 Add initial structure into GroupData class

--- src/GroupData.java
+++ src/GroupData.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class GroupData {
+    private final String path = "StudentGroupData.csv";
+
+    //TODO
+    public ArrayList<Student> getGroupData(){
+        return null;
+    }
+    //TODO
+    public String getPath(){
+        return null;
+    }
+
+    //TODO
+    private Student getGroup(){
+        return null;
+    }
 }
