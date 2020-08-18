aebd58397d80348b8fe16f0748198b77290bc115
rine <krineless@gmail.com>
2020-08-19 01:36:59 +0200

    Add initial structure into StudentData class

--- src/StudentData.java
+++ src/StudentData.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class StudentData {
+    private final String path = "StudentGroupData";
+
+    //TODO
+    public ArrayList<Student> getStudentData(){
+       return null;
+    }
+    //TODO
+    public String getPath(){
+        return null;
+    }
+
+    //TODO
+    private Student getStudent(){
+        return null;
+    }
 }
