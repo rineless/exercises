1c72a2676605087031ea703156e5baaed4b29c1a
rine <krineless@gmail.com>
2020-08-19 00:20:24 +0200

    Change jdk version from 13 into 11

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 public class Application {
 
     public static void main(String[] args){
-       System.out.println("Hello world");
+        String a = "1,Anna,Allen,f,07.11.1998,German,Hamburg,stipend,1,present,anna.allen98@gmail.com";
+        String[] b = a.split(",");
+        for(String word:b){
+            System.out.println(word);
+        }
+
     }
 }
