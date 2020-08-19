5f932fafac519c46e8d743b66edfccad7f5ab032
rine <krineless@gmail.com>
2020-08-19 23:28:07 +0200

    Modify methods parseLineToStudent and parseLineToGroup in LineParser

--- src/util/LineParser.java
+++ src/util/LineParser.java
@@ -1,16 +1,36 @@
 package util;
 
+import model.*;
+
 public interface LineParser {
     String[] parseLineToArray(String line);
-    //TODO
-    default Student parseLineToStudent(String line){
+
+    //TODO if no contact information?
+    default Student parseLineToStudent(String line) {
         String[] dataForStudent = parseLineToArray(line);
-        return new Student();
-    };
+        if (dataForStudent.length == 11)
+            return new Student(Integer.parseInt(dataForStudent[0]), dataForStudent[1]
+                    , dataForStudent[2], dataForStudent[3], dataForStudent[4], dataForStudent[5]
+                    , dataForStudent[6], dataForStudent[7], Integer.parseInt(dataForStudent[8])
+                    , dataForStudent[9], dataForStudent[10]);
+        else {
+            throw new IllegalArgumentException("Line cannot be resolved into Student. Not enough data");
+        }
+    }
+
+
     //TODO
-    default Group parseLineToGroup(String line){
+    default Group parseLineToGroup(String line) {
         String[] dataForGroup = parseLineToArray(line);
-        return new Group();
-    };
+        if (dataForGroup.length == 10)
+            return new Group(Integer.parseInt(dataForGroup[0]), dataForGroup[1], dataForGroup[2], dataForGroup[3]
+                    , dataForGroup[4], Integer.parseInt(dataForGroup[5]), Integer.parseInt(dataForGroup[6])
+                    , Integer.parseInt(dataForGroup[7]), dataForGroup[8], dataForGroup[9]);
+        else {
+            throw new IllegalArgumentException("Line cannot be resolved into Group. Not enough data");
+        }
+    }
+
+
 
 }
