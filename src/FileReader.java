8b57f55c2e9f3c5f54d35e86d43cf3b19d4c6e6d
rine <krineless@gmail.com>
2020-08-19 14:31:08 +0200

    Add abstract class Reader which is extended with FileReader

--- src/FileReader.java
+++ src/FileReader.java
@@ -1,39 +1,38 @@
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class FileReader {
-    private final String PATH;
+public class FileReader extends Reader{
+    private final String path;
 
     public FileReader(String path){
-        this.PATH = path;
+        this.path = path;
     }
 
-    public ArrayList<String[]> getDataAsList(){
-        if(getLines()!=null){
-            ArrayList<String[]> data = new ArrayList<>();
-            getLines().forEach(line -> data.add(getDataFromLine(line)));
-            return data;
+    public List<String> receiveLinesAsList(){
+        if(receiveLinesFromFileAsStream()!=null){
+            return receiveLinesFromFileAsStream().collect(Collectors.toList());
         }
         return null;
     }
 
-    private Stream<String> getLines(){
+    private Stream<String> receiveLinesFromFileAsStream(){
         try {
-            return Files.lines(Path.of(PATH));
+            return Files.lines(Path.of(path));
         } catch (IOException e) {
             System.out.println("Path given incorrect");
             return null;
         }
     }
 
-    private String[] getDataFromLine(String line){
+    /*private String[] receiveDataFromLine(String line){
         return line.split(",");
-    }
+    }*/
 
     public String getPath(){
-        return PATH;
+        return path;
     }
 
 }
---
+++ src/Reader.java
@@ -0,0 +1,5 @@
+import java.util.*;
+
+public abstract class Reader {
+    abstract public List<String> receiveLinesAsList();
+}
