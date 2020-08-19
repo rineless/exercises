def75b1074757773f1f8608e27e306ba5c515751
rine <krineless@gmail.com>
2020-08-19 18:12:38 +0200

    Add attributes and constructor to Student class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,13 +1,15 @@
 package model;
 
-import java.util.Date;
+import util.DSVParser;
+
+import java.time.LocalDate;
 
 public class Student {
     private final int id;
     private final String name;
     private final String surname;
     private final String gender;
-    private final Date birthDate;
+    private final LocalDate birthDate;
     private final String citizenship;
     private final String placeOfBirth;
     private final String typeOfContract;
@@ -15,6 +17,7 @@ public class Student {
     private final String typeOfStudying;
     private final String contractInformation;
 
+    //TODO check if arguments valid
     public Student(int id, String name, String surname, String gender, String birthDate
             , String citizenship, String placeOfBirth, String typeOfContract
             , int groupId, String typeOfStudying, String contractInformation){
@@ -22,7 +25,8 @@ public class Student {
       this.name = name;
       this.surname = surname;
       this.gender = gender;
-      //this.birthDate = birthDate; TODO
+      String[] date = new DSVParser().parseLineToArray(birthDate);
+      this.birthDate = LocalDate.of(Integer.parseInt(date[2]),Integer.parseInt(date[1]),Integer.parseInt(date[0]));
       this.citizenship = citizenship;
       this.placeOfBirth = placeOfBirth;
       this.typeOfContract = typeOfContract;
