b9cea9af6b304ebbd93e7162e3fa3524f4a40629
rine <krineless@gmail.com>
2020-08-20 14:53:58 +0200

    Clean application class

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,10 @@
+import util.FileReader;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Application {
 
     public static void main(String[] args){
-        String a = "1,Anna,Allen,f,07.11.1998,German,Hamburg,stipend,1,present,anna.allen98@gmail.com";
-        String[] b = a.split(",");
-        for(String word:b){
-            System.out.println(word);
-        }
-
     }
 }
