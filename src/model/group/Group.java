2698046f6a732f95e44c7636ae936fd1ffc5da2d
rine <krineless@gmail.com>
2020-08-20 13:56:48 +0200

    Delete constructor in Group class

--- src/model/group/Group.java
+++ src/model/group/Group.java
@@ -15,26 +15,6 @@ public class Group {
     private String[] responsibleForGroup;
     private String contactInformation;
 
-    //TODO check if attributes are valid
-    /*public Group(int id, String name, String abbreviation, String language
-            , String onlineAccess, int attendeesOnline, int attendeesPresent
-            , int maxAttendeesPresent, String responsibleForGroup, String contactInformation) {
-        this.id = id;
-        this.name = name;
-        this.abbreviation = abbreviation;
-        this.language = language;
-        if (onlineAccess.contentEquals("yes"))
-            this.onlineAccess = true;
-        else if (onlineAccess.contentEquals("no"))
-            this.onlineAccess = false;
-        else throw new IllegalArgumentException("Illegal argument: online access");
-        this.attendeesOnline = attendeesOnline;
-        this.attendeesPresent = attendeesPresent;
-        this.maxAttendeesPresent = maxAttendeesPresent;
-        this.responsibleForGroup = new SeparatedValuesParser(responsibleForGroup).parseLineToArray("");
-        this.contactInformation = contactInformation;
-    }*/
-
     public Group setId(int id) {
         this.id = id;
         return this;
