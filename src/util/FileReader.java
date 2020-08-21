45d77b25ccae8dc15c02d2506553f5bb39a71748
rine <krineless@gmail.com>
2020-08-21 22:10:29 +0200

    Remove static modifier in receiveLinesAsList methode in FileReader

--- src/util/FileReader.java
+++ src/util/FileReader.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 public class FileReader extends Reader {
 
     //TODO check exception, maybe make a bit different
-    public static List<String> receiveLinesAsList(String path) {
+    public List<String> receiveLinesAsList(String path) {
         try {
             return Files.lines(Path.of(path)).collect(Collectors.toList());
         } catch (IOException e) {
