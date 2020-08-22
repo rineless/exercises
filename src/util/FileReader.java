1ff2e320a3c9341c18187b267883096416465182
rine <krineless@gmail.com>
2020-08-22 15:02:41 +0200

    Add some new checks of argument into receiveLinesAsList method in FileReader

--- src/util/FileReader.java
+++ src/util/FileReader.java
@@ -11,8 +11,12 @@ public class FileReader extends Reader {
     //TODO check exception, maybe make a bit different
     public List<String> receiveLinesAsList(String path) {
         try {
-            return Files.lines(Path.of(path)).collect(Collectors.toList());
-        } catch (IOException e) {
+            if (Files.exists(Path.of(path)) && Files.isReadable(Path.of(path)))
+                return Files.lines(Path.of(path)).collect(Collectors.toList());
+            else {
+                throw new IllegalArgumentException("Readable file not found.");
+            }
+        } catch (IOException | IllegalArgumentException exp) {
             System.out.println("Path given incorrect. Created empty list");
             return new ArrayList<String>();
         }
