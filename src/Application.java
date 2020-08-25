46e03ca414d163c62790bdb15db0b383e7f61a17
rine <krineless@gmail.com>
2020-08-25 15:37:37 +0200

    Create addStudent method in Application

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,15 @@
 import model.group.Group;
 import model.student.Student;
+import model.student.TypeOfContract;
 import util.CSVParser;
 import util.FileReader;
 import util.SeparatedValuesParser;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -16,11 +20,23 @@ import java.util.stream.Collectors;
 public class Application {
 
     public static void main(String[] args){
+        /*Student student = receiveListOfStudents("src/data/StudentData.csv");
+        System.out.println(student.toString());*/
+        System.out.println("Students:");
         List<Student> studentList = receiveListOfStudents("src/data/StudentData.csv");
         studentList.stream().forEach(student -> System.out.println(student.toString()));
-        List<Group> groupList = receiveListOfGroups("");
-        list.add("12,Ruby,Roth,f,29.12.1998,Netherlands,Amsterdam,payable,1,present,ruby.roth@gmail.com");
-        Files.write();
+
+        System.out.println("Groups:");
+        List<Group> groupList = receiveListOfGroups("src/data/StudentGroupData.csv");
+        groupList.stream().forEach(group -> System.out.println(group.toString()));
+
+        List<String> studentsToAdd = new ArrayList<>();
+        studentsToAdd.add("13,Rubel,Bare,m,15.02.1998,German,Bremen,payable,0,present,rubel.bare@gmail.com");
+        addStudent(studentsToAdd,studentList,"src/data/StudentData.csv");
+
+
+        /*list.add("12,Ruby,Roth,f,29.12.1998,Netherlands,Amsterdam,payable,1,present,ruby.roth@gmail.com");
+        Files.write();*/
         /*Student student = new Student();
         String a = "4 06 2098";
         String[] b = a.split(" ");
@@ -44,5 +60,16 @@ public class Application {
         CSVParser parser = new CSVParser();
         lineList.remove(0);
         return lineList.stream().map(line -> parser.parseLineToGroup(line)).collect(Collectors.toList());
+    }
+
+    public static void addStudent(List<String> lines, List<Student> studentList, String path){
+        lines.stream().map(line -> new CSVParser().parseLineToStudent(line))
+                .forEach(student -> studentList.add(student));
+        try {
+            Files.write(Path.of(path),lines, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
