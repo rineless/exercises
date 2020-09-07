3264acececa4e011d54a0dd7834190e45ce2bf8d
rine <krineless@gmail.com>
2020-09-07 22:30:15 +0200

    Fix parseArrayToLine method in SeparatedValuesParser

--- src/util/parser/SeparatedValuesParser.java
+++ src/util/parser/SeparatedValuesParser.java
@@ -4,20 +4,22 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class SeparatedValuesParser implements ILineParser {
-    private final String separator;
+    private final String separator_regex;
 
     public SeparatedValuesParser(String separator_regex) {
-        this.separator = separator_regex;
+        this.separator_regex = separator_regex;
     }
 
     public String[] parseLineToArray(String line) {
-        if (line != null)
-            return line.split(separator);
+        if (line != null) {
+            return line.split(separator_regex);
+        }
         return null;
     }
 
     public String parseArrayToLine(String[] array){
-        String line = Arrays.stream(array).map(word -> word+separator).collect(Collectors.joining());
+        String separator = separator_regex.replace("\\","");
+        String line = Arrays.stream(array).map(word -> word+ separator).collect(Collectors.joining());
         return line.substring(0, line.length() - separator.length());
     }
 }
