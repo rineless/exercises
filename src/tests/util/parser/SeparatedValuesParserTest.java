61c8510e37b7c10324ad9ab954b5ea68f651d78b
rine <krineless@gmail.com>
2020-09-09 18:34:14 +0200

    Add some negative tests for lineToArray and arrayToLine methods in SeparatedValuesParserTest

--- src/tests/util/parser/SeparatedValuesParserTest.java
+++ src/tests/util/parser/SeparatedValuesParserTest.java
@@ -3,11 +3,13 @@ package util.parser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class SeparatedValuesParserTest {
 
-    //test parseLineToArray(separator)
+    /**test parseLineToArray(separator)*/
     @Test
     @DisplayName("Ordinary line with _ separator should be parsed to array")
     void shouldParseOrdinaryLineWithUnderscoreSeparatorToArray(){
@@ -53,9 +55,55 @@ public class SeparatedValuesParserTest {
     }
 
 
+    // TODO check, tried new names
+    @Test
+    void parseLineToArray_WithSpaceSeparatorParser_ShouldParseLineToArray(){
+        SeparatedValuesParser parser = new SeparatedValuesParser(" ");
+        String[] expected = new String[]{"just","check", "ordinary", "statement"};
+
+        String[] actual = parser.parseLineToArray("just check ordinary statement ");
+
+        Assertions.assertArrayEquals(expected, actual, "Expected parsed line to array");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {","})
+    @DisplayName("Empty line should be parsed to array (with comma separator parser)")
+    void parseLineToArray_WithCommaSeparatorParser_ShouldParseEmptyLineToEmptyArray(String separator_regex){
+        SeparatedValuesParser parser = new SeparatedValuesParser(separator_regex);
+        String[] expected = new String[]{};
+
+        String[] actual = parser.parseLineToArray("");
+
+        Assertions.assertArrayEquals(expected, actual, "Expected parsed empty line into empty array");
+    }
+
+    @Test
+    @DisplayName("Empty line should be parsed to array (with space separator parser)")
+    void parseLineToArray_WithSpaceSeparatorParser_ShouldParseEmptyLineToArray(){
+        SeparatedValuesParser parser = new SeparatedValuesParser(" ");
+        String[] expected = new String[]{};
+
+        String[] actual = parser.parseLineToArray("");
+
+        Assertions.assertArrayEquals(expected, actual, "Expected parsed empty line into empty array");
+    }
+
+    @Test
+    @DisplayName("Null should be parsed to empty array")
+    void parseLineToArray_WithSpaceSeparatorParser_ShouldParseNullToEmptyArray(){
+        SeparatedValuesParser parser = new SeparatedValuesParser(" ");
+        String[] expected = new String[]{};
+
+        String[] actual = parser.parseLineToArray(null);
+
+        Assertions.assertArrayEquals(expected, actual, "Expected parsed null into empty array");
+    }
+
     //TODO make negative tests for lineToArray
 
-    // test parseArrayToLine(separator)
+    /** test parseArrayToLine(separator)*/
+
     @Test
     @DisplayName("Array should be parsed to line with _ separator")
     void shouldParseArrayToLineWithUnderscoreSeparator(){
@@ -100,5 +148,29 @@ public class SeparatedValuesParserTest {
         Assertions.assertEquals(expected, actual, "Expected parsed array into line");
     }
 
+    @Test
+    @DisplayName("Empty array should be parsed to empty line")
+    void parseArrayToLine_WithSpaceSeparatorParser_ShouldParseEmptyArrayToEmptyLine(){
+        SeparatedValuesParser parser = new SeparatedValuesParser(" ");
+        String expected = "";
+
+        String actual = parser.parseArrayToLine(new String[]{});
+
+        Assertions.assertEquals(expected, actual, "Expected parsed empty array into empty line");
+    }
+
+    @Test
+    @DisplayName("Null should be parsed to empty line")
+    void parseArrayToLine_WithSpaceSeparatorParser_ShouldParseNullToEmptyArray(){
+        SeparatedValuesParser parser = new SeparatedValuesParser(" ");
+        String expected = "";
+
+        String actual = parser.parseArrayToLine(null);
+
+        Assertions.assertEquals(expected, actual, "Expected parsed null into empty line");
+    }
+
     //TODO make negative tests for arrayToLine
+
+
 }
