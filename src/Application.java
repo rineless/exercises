15c2163a795495d56cd4a064e09a3d492ec30af0
rine <krineless@gmail.com>
2020-09-18 14:36:21 +0200

    Change path to relative in Application

--- src/Application.java
+++ src/Application.java
@@ -1,32 +1,27 @@
 import model.group.Group;
 import model.student.Student;
-import model.student.TypeOfContract;
-import util.CSVParser;
-import util.FileReader;
-import util.SeparatedValuesParser;
+import util.parser.CSVParser;
+import util.reader.FileReader;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.sql.SQLOutput;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Application {
 
     public static void main(String[] args){
+        Path current = Path.of(".");
 
         System.out.println("Students:");
-        List<Student> studentList = receiveListOfStudents("src/data/StudentData.csv");
+        List<Student> studentList = receiveListOfStudents(current.toString() + "/src/data/StudentData.csv");
         studentList.stream().forEach(student -> System.out.println(student.toString()));
 
         System.out.println("Groups:");
-        List<Group> groupList = receiveListOfGroups("src/data/StudentGroupData.csv");
+        List<Group> groupList = receiveListOfGroups(current.toString() + "/src/data/StudentGroupData.csv");
         groupList.stream().forEach(group -> System.out.println(group.toString()));
 
         List<String> studentsToAdd = new ArrayList<>();
