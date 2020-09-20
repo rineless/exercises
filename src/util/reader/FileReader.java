485c00d6697e5edaffb82ef43b6dc8c658b4fd8c
rine <krineless@gmail.com>
2020-09-20 02:18:22 +0200

    Fixed #16 Add check if path is pointing on file in receiveLinesAsList method

--- src/util/reader/FileReader.java
+++ src/util/reader/FileReader.java
@@ -7,16 +7,17 @@ import java.util.stream.Collectors;
 
 public class FileReader extends Reader {
 
-    //TODO check exceptions, maybe make a bit different
+
     public List<String> receiveLinesAsList(String path) {
         try {
-            if (Files.exists(Path.of(path)) && Files.isReadable(Path.of(path)))
+            if (Files.exists(Path.of(path)) && Files.isRegularFile(Path.of(path))
+                    && Files.isReadable(Path.of(path)))
                 return Files.lines(Path.of(path)).collect(Collectors.toList());
             else {
                 throw new IllegalArgumentException("Readable file not found.");
             }
         } catch (IOException | IllegalArgumentException exp) {
-            System.out.println("Path given incorrect. Created empty list"); //TODO check excp
+            System.out.println("Path given incorrect. Created empty list");
             return new ArrayList<String>();
         }
     }
