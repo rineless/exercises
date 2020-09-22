2b608b617a058d84ce32c6e7842b620a1aeb49a2
rine <krineless@gmail.com>
2020-09-23 00:17:26 +0200

    Fix bug in findByName method in PathFinder

--- src/util/finder/PathFinder.java
+++ src/util/finder/PathFinder.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class PathFinder {
 
     public static void findByName(String name, File rootDirectory, List<String> paths)
-            throws IllegalArgumentException{
-        if(name != null | rootDirectory != null | paths != null) {
-            if(rootDirectory.isDirectory()) {
-
-                for (File file : rootDirectory.listFiles()) {
-                    if (file.isDirectory())
-                        findByName(name, file, paths);
-
-                    if (file.getName().contentEquals(name))
-                        paths.add(file.getAbsolutePath());
-                }
-            }
+            throws IllegalArgumentException {
+        if (name == null | rootDirectory == null | paths == null) {
+            throw new IllegalArgumentException("Null cannot be resolved into name or rootDirectory or paths");
+        }
+        if (!rootDirectory.isDirectory()) {
+            throw new IllegalArgumentException(" Given rootDirectory is not a directory");
         }
 
-        throw new IllegalArgumentException("Null cannot be resolved into name or rootDirectory or paths" +
-                "; Or given rootDirectory is not a directory");
+        for (File file : rootDirectory.listFiles()) {
+            if (file.isDirectory())
+                findByName(name, file, paths);
+
+            if (file.getName().contentEquals(name))
+                paths.add(file.getAbsolutePath());
+        }
+
     }
 }
