98eed280a3f328e35042131fdca7dbe272a4a42d
rine <krineless@gmail.com>
2020-10-04 17:16:16 +0200

    Modified CSVStudentsRepository

--- src/repository/CSVStudentsRepository.java
+++ src/repository/CSVStudentsRepository.java
@@ -1,34 +1,37 @@
 package repository;
 
 import model.student.Student;
+import util.finder.PathFinder;
 import util.parser.CSVParser;
 import util.parser.ILineParser;
 import util.reader.FileReader;
+import util.validation.StudentValidation;
+import util.writer.FileWriter;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class CSVStudentsRepository implements StudentsRepository {
 
     private final FileReader reader;
+    private final FileWriter writer;
     private final ILineParser parser;
     private final String studentDataPath = "data/StudentData.csv";
 
     public CSVStudentsRepository() {
         reader = new FileReader();
+        writer = new FileWriter();
         parser = new CSVParser();
 
     }
 
     public List<Student> getAll() {
 
-        List<Student> studentData = reader.receiveLinesAsList(studentDataPath).stream().skip(1)
+        List<Student> studentData = reader.receiveLinesAsList(PathFinder.findFromResources(studentDataPath)).stream().skip(1)
                 .map(line -> parser.parseLineToStudent(line)).collect(Collectors.toList());
 
         return studentData.stream().collect(Collectors.toList());
@@ -46,62 +49,36 @@ public class CSVStudentsRepository implements StudentsRepository {
         return student;
     }
 
-    public void add(Student student) throws IllegalArgumentException{
+    public void add(Student student) throws IllegalArgumentException {
         if (student != null) {
 
-            if (student.getName() != null & student.getSurname() != null & student.getGender() != null
-                    & student.getBirthDate() != null & student.getCitizenship() != null & student.getPlaceOfBirth() != null
-                    & student.getTypeOfContract() != null & student.getTypeOfStudying() != null & student.getContactInformation() != null) {//TODO please follow the Single Responsibility principle and move the validation to an appropriate class
+            if (StudentValidation.isValid(student)) {//DONE please follow the Single Responsibility principle and move the validation to an appropriate class
 
-                try {
-                    Files.write(studentDataPath, (parser.parseStudentToLine(student) + "\n").getBytes()
-                            , StandardOpenOption.APPEND);  //TODO move this code to FileReader and rename it to thr File ReaderWriter
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                studentData.add(student);
+                writer.appendLine(parser.parseStudentToLine(student) + "\n", PathFinder.findFromResources(studentDataPath));//DONE move this code to FileReader and rename it to thr File ReaderWriter
 
             }
         }
     }
 
-        public void update () {
-            if (studentData == null) {
-                throw new IllegalArgumentException("Student repository is not created yet. Cannot update null list");
-            }
-
-            reader.receiveLinesAsList(studentDataPath).stream().skip(1)
-                    .map(line -> parser.parseLineToStudent(line))
-                    .filter(student -> {
-                        for (Student studentFromCSVFile : studentData) {
-                            if (studentFromCSVFile.equals(student))
-                                return false;
-                        }
-                        return true;
-                    }).forEach(student -> studentData.add(student));
-
-
+    public void update() {
+        if (studentData == null) {
+            throw new IllegalArgumentException("Student repository is not created yet. Cannot update null list");
         }
 
-        public void delete (Student student){
+        reader.receiveLinesAsList(studentDataPath).stream().skip(1)
+                .map(line -> parser.parseLineToStudent(line))
+                .filter(student -> {
+                    for (Student studentFromCSVFile : studentData) {
+                        if (studentFromCSVFile.equals(student))
+                            return false;
+                    }
+                    return true;
+                }).forEach(student -> studentData.add(student));
 
-            if (studentData == null) {
-                throw new IllegalArgumentException("Student repository is not created yet. Cannot delete Student from null list.");
-            }
 
-            try {
-
-                Student foundStudent = studentData.stream().filter(studentFromDatabase -> studentFromDatabase.equals(student))
-                        .findFirst().get();
-                studentData.remove(foundStudent);
-
-                Files.write(studentDataPath, studentData.stream()
-                        .map(eachStudent -> parser.parseStudentToLine(eachStudent)).collect(Collectors.toList()));
-            } catch (NoSuchElementException | NullPointerException exception) {
-                System.out.println("Student not found. Cannot delete");
-            } catch (IOException exception) {
-                System.out.println("Path not found");
-            }
-        }
     }
+
+    public void delete(Student student) {
+        writer.deleteLine(parser.parseStudentToLine(student), PathFinder.findFromResources(studentDataPath));
+    }
+}
