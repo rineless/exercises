eb1b3b7c257877bf96abf89ca0e413c70fc3db1b
rine <krineless@gmail.com>
2020-10-05 16:57:51 +0200

    Use Objects.nonNull method in StudentValidation

--- src/util/validation/StudentValidation.java
+++ src/util/validation/StudentValidation.java
@@ -2,11 +2,13 @@ package util.validation;
 
 import model.student.Student;
 
+import java.util.Objects;
+
 public class StudentValidation implements Validation{
 
     @Override
     public boolean isValid(Object student) {
-        if (student != null) {
+        if (Objects.nonNull(student)) {
             if (student instanceof Student) {
                 return isValid((Student) student);
             }
@@ -15,10 +17,10 @@ public class StudentValidation implements Validation{
     }
 
     public static boolean isValid(Student student) {
-        if (student != null) {
-            if (student.getName() != null & student.getSurname() != null & student.getGender() != null
-                    & student.getBirthDate() != null & student.getCitizenship() != null & student.getPlaceOfBirth() != null
-                    & student.getTypeOfContract() != null & student.getTypeOfStudying() != null & student.getContactInformation() != null)
+        if (Objects.nonNull(student)) {
+            if (Objects.nonNull(student.getName()) & Objects.nonNull(student.getSurname()) & Objects.nonNull(student.getGender())
+                    & Objects.nonNull(student.getBirthDate()) & Objects.nonNull(student.getCitizenship()) & Objects.nonNull(student.getPlaceOfBirth())
+                    & Objects.nonNull(student.getTypeOfContract()) & Objects.nonNull(student.getTypeOfStudying()) & Objects.nonNull(student.getContactInformation()))
                 return true;
         }
         return false;
