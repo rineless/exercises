d40059bec2bd606a018c3287b55790ad8ef2c285
rine <krineless@gmail.com>
2020-10-06 02:14:11 +0200

    Add methods to GroupsRepository and CSVGroupsRepository

--- src/repository/CSVGroupsRepository.java
+++ src/repository/CSVGroupsRepository.java
@@ -1,4 +1,27 @@
 package repository;
 
+import model.group.Group;
+
+import java.util.List;
+
 public class CSVGroupsRepository implements GroupsRepository {
+    public List<Group> getAll(){
+        return null;
+    };
+
+    public Group getById(int id){
+        return null;
+    };
+
+    public void add(Group group){
+
+    };
+
+    public void update(Group group){
+
+    };
+
+    public void delete(Group group){
+
+    };
 }
