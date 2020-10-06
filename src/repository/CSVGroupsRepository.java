bdac6e91c8d88ebad2a128bb0e96e49257c17da8
rine <krineless@gmail.com>
2020-10-06 02:21:35 +0200

    Add attributes to CSVGroupsRepository

--- src/repository/CSVGroupsRepository.java
+++ src/repository/CSVGroupsRepository.java
@@ -1,10 +1,25 @@
 package repository;
 
 import model.group.Group;
+import util.parser.CSVParser;
+import util.parser.ILineParser;
+import util.reader.FileReader;
+import util.writer.FileWriter;
 
 import java.util.List;
 
 public class CSVGroupsRepository implements GroupsRepository {
+    private final FileReader reader;
+    private final FileWriter writer;
+    private final ILineParser parser;
+    private final String groupsDataPath = "data/StudentGroupData.csv";
+
+    public CSVGroupsRepository(){
+        reader = new FileReader();
+        writer = new FileWriter();
+        parser = new CSVParser();
+    }
+
     public List<Group> getAll(){
         return null;
     };
