a7d8ce083b2ff63ddf86c9dee1313297b30b3b96
rine <krineless@gmail.com>
2020-10-10 15:12:22 +0200

    Create methods to fill and clear file before and after each test. Create positive test for rewriteLine method in FileWriterTest

--- src/tests/util/writer/FileWriterTest.java
+++ src/tests/util/writer/FileWriterTest.java
@@ -1,5 +1,42 @@
 package util.writer;
 
-public class FileWriterTest {
+import org.junit.jupiter.api.*;
+import util.finder.PathFinder;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileWriterTest {
+    Path file = PathFinder.findFromResources("util/writer/file.txt");
+
+    @BeforeEach
+    public void fillFileWithSomeLines() throws IOException { //TODO exceptions
+        List<String> lines = new LinkedList<>();
+        lines.add("Hey!");
+        lines.add("You");
+        lines.add("are");
+        lines.add("awesome ;)");
+        Files.write(file, lines);
+    }
+
+    @AfterEach
+    public void clearFile() throws IOException { //TODO exceptions
+        Files.write(file, "".getBytes());
+    }
+
+    @Test
+    @DisplayName("Line should be rewritten in file")
+    public void shouldRewriteLine() throws IOException { //TODO exception
+        FileWriter writer = new FileWriter();
+        String expected = "Hey!\n"+"We\n"+"are\n"+"awesome ;)\n";
+
+        writer.rewriteLine("We", 1, file);
+        String actual = Files.lines(file).map(line -> line + "\n").collect(Collectors.joining());
+
+        Assertions.assertEquals(expected, actual, "Expected rewritten line in file");
+    }
 }
