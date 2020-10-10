a99b5066e1dd248b38c19392c570d90d299ad543
rine <krineless@gmail.com>
2020-10-10 14:49:43 +0200

    Create positive tests for getAll, getById, addStudent methods in CSVStudentsRepositoryTest

--- src/tests/repository/CSVStudentsRepositoryTest.java
+++ src/tests/repository/CSVStudentsRepositoryTest.java
@@ -8,23 +8,19 @@ import org.junit.jupiter.api.*;
 import util.finder.PathFinder;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CSVStudentsRepositoryTest {
     static Path repository = PathFinder.findFromResources("data/StudentData.csv");
 
-    @BeforeAll
-    public static void renameOriginalRepository() throws IOException { //TODO exception
-        Files.move(repository, repository.resolveSibling("OStudentData.csv"));
-    }
-
     @BeforeEach
-    public void createTestRepository() throws IOException { //TODO exception
-        Files.createFile(repository);
+    public void fillTestRepository() throws IOException { //TODO exception
         List<String> lines = new LinkedList<>();
         lines.add("ID,Name,Surname,Gender,Birth date,Citizenship,Place of Birth,Type of contract,Group ID,Type of Studying,Contact information");
         lines.add("1,Anna,Allen,f,07.11.1998,German,Hamburg,stipend,1,present,anna.allen98@gmail.com");
@@ -34,8 +30,8 @@ public class CSVStudentsRepositoryTest {
     }
 
     @AfterEach
-    public void deleteTestRepository() throws IOException { //TODO exception
-        Files.delete(repository);
+    public void clearTestRepository() throws IOException { //TODO exception
+        Files.write(repository, "".getBytes());
     }
 
     @Test
@@ -60,9 +56,54 @@ public class CSVStudentsRepositoryTest {
         Assertions.assertIterableEquals(expected, actual, "Expected returned list of all students");
     }
 
-    @AfterAll
-    public static void returnOriginalRepositoryName() throws IOException { //TODO exception
-        Path originalRepository = PathFinder.findFromResources("data/OStudentData.csv");
-        Files.move(originalRepository, originalRepository.resolveSibling("StudentData.csv"));
+    @Test
+    @DisplayName("Student with inputted id should be returned")
+    public void getById_ShouldReturnStudent(){
+        Student expected = new Student().setId(2).setName("Peter").setSurname("Tailor").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999,6,25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(2).setTypeOfStudying(TypeOfStudying.PRESENT)
+                .setContactInformation("petter99tailor@gmail.com");
+
+        Student actual = new CSVStudentsRepository().getById(2);
+
+        Assertions.assertEquals(expected, actual, "Expected returned student with inputted id");
     }
+
+    @Test
+    @DisplayName("Student should be added to repository")
+    public void addStudent_ShouldAddStudentToRepository() throws IOException { //TODO exception
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository();
+        String expected = "ID,Name,Surname,Gender,Birth date,Citizenship,Place of Birth,Type of contract,Group ID,Type of Studying,Contact information\n" +
+                "1,Anna,Allen,f,07.11.1998,German,Hamburg,stipend,1,present,anna.allen98@gmail.com\n" +
+                "2,Peter,Tailor,m,25.06.1999,German,Bremen,payable,2,present,petter99tailor@gmail.com\n" +
+                "3,Alice,Cook,f,14.04.1999,Ungarn,Paks,payable,1,online,alicook@gmail.com\n"+
+                "4,Gerald,Anond,MALE,13.6.1998,German,Dresden,STIPEND,3,ONLINE,gerald.anond@gmail.com\n";
+
+        studentsRepository.add(new Student().setId(4).setName("Gerald").setSurname("Anond").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1998,6,13)).setCitizenship("German").setPlaceOfBirth("Dresden")
+                .setTypeOfContract(TypeOfContract.STIPEND).setGroupId(3).setTypeOfStudying(TypeOfStudying.ONLINE)
+                .setContactInformation("gerald.anond@gmail.com"));
+        String actual = Files.lines(repository).map(line -> line + "\n").collect(Collectors.joining());
+
+        Assertions.assertEquals(expected, actual, "Expected adding student to repository");
+    }
+
+    @Test
+    @DisplayName("Student information should be updated")
+    public void updateStudent_ShouldUpdateStudentInformation() throws IOException { //TODO exception
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository();
+        String expected = "ID,Name,Surname,Gender,Birth date,Citizenship,Place of Birth,Type of contract,Group ID,Type of Studying,Contact information\n" +
+                "1,Anna,Allen,f,07.11.1998,German,Hamburg,stipend,1,present,anna.allen98@gmail.com\n" +
+                "2,Pettery,Trailor,MALE,25.6.1999,German,Bremen,PAYABLE,1,PRESENT,pettery99trailor@gmail.com\n" +
+                "3,Alice,Cook,f,14.04.1999,Ungarn,Paks,payable,1,online,alicook@gmail.com\n";
+
+        studentsRepository.update(new Student().setId(2).setName("Pettery").setSurname("Trailor").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999,6,25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(1).setTypeOfStudying(TypeOfStudying.PRESENT)
+                .setContactInformation("pettery99trailor@gmail.com"));
+        String actual = Files.lines(repository).map(line -> line + "\n").collect(Collectors.joining());
+
+        Assertions.assertEquals(expected, actual, "Expected updating existing student");
+    }
+
 }
