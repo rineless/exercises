4ce3c489ae593e4c670f9d658af8f59902ba3edf
rine <krineless@gmail.com>
2020-10-13 16:33:04 +0200

    Add some methods in IGroupsService

--- src/service/IGroupsService.java
+++ src/service/IGroupsService.java
@@ -1,11 +1,31 @@
 package service;
 
 import model.group.Group;
+import model.group.GroupNames;
 import model.student.Student;
 
 import java.util.List;
+import java.util.Locale;
 
 public interface IGroupsService {
+
+    List<Group> getAll();
+
+    Group getById(int id);
+
+    void add(Group group);
+
+    void update(Group group);
+
+    void delete(Group group);
+
     List<Student> findAllStudentsInGroup(int id);
+
     List<Group> findFullGroups();
+
+    List<Group> findAllGroupsByLanguage(Locale language);
+
+    List<Group> findAllGroupsWithOnlineAccess(boolean onlineAccess);
+
+    List<Group> findAllGroupsByName(GroupNames groupName);
 }
