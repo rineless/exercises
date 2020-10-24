d3d98c0b47c8955d6429e9a734e3184fb65621aa
rine <krineless@gmail.com>
2020-10-24 19:11:07 +0200

    Modify Application by using only ConsoleUserCommunicator; in Application

--- src/Application.java
+++ src/Application.java
@@ -1,93 +1,11 @@
-import model.group.Group;
-import model.student.Gender;
-import model.student.Student;
-import model.student.TypeOfContract;
-import model.student.TypeOfStudying;
-import util.parser.CSVParser;
-import util.reader.FileReader;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import menu.ConsoleUserCommunicator;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        String studentDataPath = receiveFilePath("data/StudentData.csv");
-        String groupDataPath = receiveFilePath("data/StudentGroupData.csv");
-
-        System.out.println("Students:");
-        List<Student> studentList = receiveListOfStudents(studentDataPath);
-        studentList.stream().forEach(student -> System.out.println(student.toString()));
-
-        System.out.println("Groups:");
-        List<Group> groupList = receiveListOfGroups(groupDataPath);
-        groupList.stream().forEach(group -> System.out.println(group.toString()));
-
-        List<Student> studentsToAdd = new ArrayList<>();
-        studentsToAdd.add(new Student().setId(13).setName("Rubel").setSurname("Bare").setGender(Gender.MALE)
-                .setBirthDate(LocalDate.of(1998, 2, 15)).setCitizenship("German").setPlaceOfBirth("Bremen")
-                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(5).setTypeOfStudying(TypeOfStudying.PRESENT).setContactInformation("rubel.bare@gmail.com"));
-        addStudents(studentsToAdd, studentList, studentDataPath);
-
-
-        //TODO find what to do with new abbreviations
-        /*List<String> groupsToAdd = new ArrayList<>();
-        groupsToAdd.add("4,Analysis,Anl,english,no,0,3,15,Karol Maier,karol.maier@myuni.de");
-        addGroup(groupsToAdd,groupList,"src/data/StudentGroupData.csv");*/
-
-
-
-    }
-
-    public static String receiveFilePath(String name){
-        URL url = new Application().getClass().getClassLoader().getResource(name);
-        return new File(url.getPath()).toString();
-    }
-
-    public static List<Student> receiveListOfStudents(String path) {
-        FileReader studentReader = new FileReader();
-
-        return studentReader.receiveLinesAsList(path).stream().skip(1)
-                .map(line -> new CSVParser().parseLineToStudent(line)).collect(Collectors.toList());
-    }
-
-    public static List<Group> receiveListOfGroups(String path) {
-        FileReader groupReader = new FileReader();
-
-        return groupReader.receiveLinesAsList(path).stream().skip(1)
-                .map(line -> new CSVParser().parseLineToGroup(line)).collect(Collectors.toList());
-    }
-
-    public static void addStudents(List<Student> studentsToAdd, List<Student> studentList, String path) {
-        studentsToAdd.stream().forEach(student -> studentList.add(student));
-        try {
-            Files.write(Path.of(path), studentsToAdd.stream()
-                            .map(student -> new CSVParser().parseStudentToLine(student)).collect(Collectors.toList())
-                    , StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    //TODO check, finalize
-    public static void addGroup(List<String> lines, List<Group> groupList, String path) {
-        lines.stream().map(line -> new CSVParser().parseLineToGroup(line))
-                .forEach(group -> groupList.add(group));
-        try {
-            Files.write(Path.of(path), lines, StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        ConsoleUserCommunicator userCommunicator = new ConsoleUserCommunicator();
+        userCommunicator.applyUserCommunicationLogic();
 
     }
 }
