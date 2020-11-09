b255a035e77efc6d5a0550fd3166dbab035e1653
rine <krineless@gmail.com>
2020-11-09 23:06:37 +0100

    Fix bug in findFromResources(String path) method. Create FileSystem only once for resources ;in PathFinder

--- src/main/java/util/finder/PathFinder.java
+++ src/main/java/util/finder/PathFinder.java
@@ -6,13 +6,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
+import static java.nio.file.FileSystems.getFileSystem;
+
 public class PathFinder {
+    private static FileSystem fileSystemForResourcesFolder;
 
     public static void findByName(String name, File rootDirectory, List<String> paths)
             throws IllegalArgumentException {
@@ -33,22 +37,24 @@ public class PathFinder {
 
     }
 
-    public static Path findFromResources(String path){
+    public static Path findFromResources(String path) {
         try {
             URL url = new PathFinder().getClass().getClassLoader().getResource(path);
-            if(Objects.nonNull(url)) {
+            if (Objects.nonNull(url)) {
                 if (url.toString().contains("!")) {
                     String[] array = url.toURI().toString().split("!");
-                    FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), new HashMap<>());
-                    return fs.getPath(array[1]);
+                    if (Objects.isNull(fileSystemForResourcesFolder)) {
+                        fileSystemForResourcesFolder = FileSystems.newFileSystem(URI.create(array[0]), new HashMap<>());
+                    } else
+                        fileSystemForResourcesFolder = FileSystems.getFileSystem(URI.create(array[0]));
+                    return fileSystemForResourcesFolder.getPath(array[1]);
                 } else
                     return Path.of(url.toURI());
-            }
-            else{
+            } else {
                 System.out.println("File not found. Return empty path");
                 return Path.of("");
             }
-        } catch(URISyntaxException | IOException exp){
+        } catch (URISyntaxException | IOException exp) {
             System.out.println(exp.getMessage());
             System.out.println("Return empty path");
             return Path.of("");
