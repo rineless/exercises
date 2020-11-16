e0dcd60c3d748054b6085bc45555886e5c5dcadf
rine <krineless@gmail.com>
2020-11-16 01:56:28 +0100

    Add comments for adding new types ; in TypesOfContract and TypesOfStudying

--- src/main/java/model/student/TypeOfContract.java
+++ src/main/java/model/student/TypeOfContract.java
@@ -1,5 +1,6 @@
 package model.student;
 
+//By adding new contract types, change Student.java setTypeOfContract method and properties for student (typeOfContract_regex)
 public enum TypeOfContract {
     STIPEND, PAYABLE
 }
