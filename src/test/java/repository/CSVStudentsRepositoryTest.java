a68ffdd8b5129da0dccfb1ca67362774dd733ba9
rine <krineless@gmail.com>
2020-11-26 15:41:50 +0100

    Mock ILineParser. ; in CSVStudentsRepositoryTest.java

--- src/test/java/repository/CSVStudentsRepositoryTest.java
+++ src/test/java/repository/CSVStudentsRepositoryTest.java
@@ -18,7 +18,6 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.validateMockitoUsage;
@@ -47,25 +46,24 @@ public class CSVStudentsRepositoryTest {
         lines.add("3,Alice,Cook,FEMALE,14.4.1999,Ungarn,Paks,PAYABLE,1,ONLINE,alicook@gmail.com");
         lenient().when(mockReader.receiveLinesAsList(repository)).thenReturn(lines);
 
-        Mockito.when(mockParser
+        lenient().when(mockParser
                 .parseLineToStudent("1,Anna,Allen,FEMALE,7.11.1998,German,Hamburg,STIPEND,1,PRESENT,anna.allen98@gmail.com"))
                 .thenReturn(new Student().setId(1).setName("Anna").setSurname("Allen").setGender(Gender.FEMALE)
                         .setBirthDate(LocalDate.of(1998, 11, 7)).setCitizenship("German").setPlaceOfBirth("Hamburg")
                         .setTypeOfContract(TypeOfContract.STIPEND).setGroupId(1).setTypeOfStudying(TypeOfStudying.PRESENT)
                         .setContactInformation("anna.allen98@gmail.com"));
-        Mockito.when(mockParser
+        lenient().when(mockParser
                 .parseLineToStudent("2,Peter,Tailor,MALE,25.6.1999,German,Bremen,PAYABLE,2,PRESENT,petter99tailor@gmail.com"))
                 .thenReturn(new Student().setId(2).setName("Peter").setSurname("Tailor").setGender(Gender.MALE)
                         .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
                         .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(2).setTypeOfStudying(TypeOfStudying.PRESENT)
                         .setContactInformation("petter99tailor@gmail.com"));
-        Mockito.when(mockParser
+        lenient().when(mockParser
                 .parseLineToStudent("3,Alice,Cook,FEMALE,14.4.1999,Ungarn,Paks,PAYABLE,1,ONLINE,alicook@gmail.com"))
                 .thenReturn(new Student().setId(3).setName("Alice").setSurname("Cook").setGender(Gender.FEMALE)
                         .setBirthDate(LocalDate.of(1999, 4, 14)).setCitizenship("Ungarn").setPlaceOfBirth("Paks")
                         .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(1).setTypeOfStudying(TypeOfStudying.ONLINE)
                         .setContactInformation("alicook@gmail.com"));
-
 
     }
 
@@ -124,10 +122,15 @@ public class CSVStudentsRepositoryTest {
    @Test
     @DisplayName("Student should be added to repository")
     public void addStudent_ShouldAddStudentToRepository() {
-        Mockito.when(mockWriter.appendLine(Mockito.anyString(), Mockito.eq(repository))).thenReturn(true);
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
+       Mockito.when(mockParser.parseStudentToLine(new Student().setId(4).setName("Gerald").setSurname("Anond").setGender(Gender.MALE)
+               .setBirthDate(LocalDate.of(1998, 6, 13)).setCitizenship("German").setPlaceOfBirth("Dresden")
+               .setTypeOfContract(TypeOfContract.STIPEND).setGroupId(3).setTypeOfStudying(TypeOfStudying.ONLINE)
+               .setContactInformation("gerald.anond@gmail.com")))
+               .thenReturn("4,Gerald,Anond,MALE,13.6.1998,German,Dresden,STIPEND,3,ONLINE,gerald.anond@gmail.com");
+       Mockito.when(mockWriter.appendLine(Mockito.anyString(), Mockito.eq(repository))).thenReturn(true);
+       CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
-        studentsRepository.add(new Student().setId(4).setName("Gerald").setSurname("Anond").setGender(Gender.MALE)
+       studentsRepository.add(new Student().setId(4).setName("Gerald").setSurname("Anond").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1998, 6, 13)).setCitizenship("German").setPlaceOfBirth("Dresden")
                 .setTypeOfContract(TypeOfContract.STIPEND).setGroupId(3).setTypeOfStudying(TypeOfStudying.ONLINE)
                 .setContactInformation("gerald.anond@gmail.com"));
@@ -141,7 +144,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("Null should not be added to repository")
     public void addStudent_WithNullInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.add(null);
         Mockito.verify(mockWriter, Mockito.never().description("Expected by null input unchanged repository." +
@@ -153,7 +156,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("NonValid student should not be added to repository")
     public void addStudent_WithNotValidStudentInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.add(new Student().setId(4).setSurname("Anond").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1998, 6, 13)).setCitizenship("German").setPlaceOfBirth("Dresden")
@@ -169,8 +172,13 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("Student information should be updated")
     public void updateStudent_ShouldUpdateStudentInformation() {
+        Mockito.when(mockParser.parseStudentToLine(new Student().setId(2).setName("Pettery").setSurname("Trailor").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(1).setTypeOfStudying(TypeOfStudying.PRESENT)
+                .setContactInformation("pettery99trailor@gmail.com")))
+                .thenReturn("2,Pettery,Trailor,MALE,25.6.1999,German,Bremen,PAYABLE,1,PRESENT,pettery99trailor@gmail.com");
         lenient().when(mockWriter.appendLine(Mockito.anyString(), Mockito.eq(repository))).thenReturn(true);
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.update(new Student().setId(2).setName("Pettery").setSurname("Trailor").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
@@ -187,7 +195,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("Not existing student cannot be updated")
     public void updateStudent_WithNotExistingStudentInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.update(new Student().setId(7).setSurname("Anond").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1998, 6, 13)).setCitizenship("German").setPlaceOfBirth("Dresden")
@@ -203,7 +211,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("Null cannot be updated")
     public void updateStudent_WithNullInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.update(null);
 
@@ -216,7 +224,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName(" By not valid student input, student cannot be updated")
     public void updateStudent_WithNotValidStudent_ShouldChangeNothingInRepository(){
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.update(new Student().setId(2).setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1998, 6, 13)).setCitizenship("German").setPlaceOfBirth("Dresden")
@@ -232,7 +240,12 @@ public class CSVStudentsRepositoryTest {
     @DisplayName("Student should be deleted from repository")
     public void deleteStudent_ShouldDeleteStudentFromRepository(){
         lenient().when(mockWriter.deleteLine(Mockito.anyString(), Mockito.eq(repository))).thenReturn(true);
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        Mockito.when(mockParser.parseStudentToLine(new Student().setId(2).setName("Peter").setSurname("Tailor").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(2).setTypeOfStudying(TypeOfStudying.PRESENT)
+                .setContactInformation("petter99tailor@gmail.com")))
+                .thenReturn("2,Peter,Tailor,MALE,25.6.1999,German,Bremen,PAYABLE,2,PRESENT,petter99tailor@gmail.com");
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.delete(new Student().setId(2).setName("Peter").setSurname("Tailor").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
@@ -247,7 +260,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("Null cannot be deleted from repository")
     public void deleteStudent_WithNullInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.delete(null);
 
@@ -260,7 +273,7 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("By not valid student input nothing should be deleted from repository")
     public void deleteStudent_WithNotValidStudentInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.delete(new Student().setId(2).setSurname("Tailor").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
@@ -276,7 +289,12 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("By not existing student input nothing should be deleted from repository")
     public void deleteStudent_WithNotExistingStudentInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        Mockito.when(mockParser.parseStudentToLine(new Student().setId(7).setName("Tailor").setSurname("Jh").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setTypeOfStudying(TypeOfStudying.PRESENT).setGroupId(2)
+                .setContactInformation("petter99tailor@gmail.com")))
+                .thenReturn("7,Tailor,Jh,MALE,25.6.1999,German,Bremen,PAYABLE,2,PRESENT,petter99tailor@gmail.com");
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.delete(new Student().setId(7).setName("Tailor").setSurname("Jh").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1999, 6, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
@@ -292,7 +310,12 @@ public class CSVStudentsRepositoryTest {
     @Test
     @DisplayName("By student with some modified data input nothing should be deleted from repository")
     public void deleteStudent_WithStudentWithModifiedDataInput_ShouldChangeNothingInRepository() {
-        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter);
+        Mockito.when(mockParser.parseStudentToLine(new Student().setId(2).setName("Peterry").setSurname("Tailorre").setGender(Gender.MALE)
+                .setBirthDate(LocalDate.of(1999, 7, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
+                .setTypeOfContract(TypeOfContract.PAYABLE).setGroupId(2).setTypeOfStudying(TypeOfStudying.PRESENT)
+                .setContactInformation("petter99tailor@gmail.com")))
+                .thenReturn("2,Peterry,Tailorre,MALE,25.7.1999,German,Bremen,PAYABLE,2,PRESENT,petter99tailor@gmail.com");
+        CSVStudentsRepository studentsRepository = new CSVStudentsRepository(mockReader, mockWriter, mockParser);
 
         studentsRepository.delete(new Student().setId(2).setName("Peterry").setSurname("Tailorre").setGender(Gender.MALE)
                 .setBirthDate(LocalDate.of(1999, 7, 25)).setCitizenship("German").setPlaceOfBirth("Bremen")
